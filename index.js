cf1448a3510c51736dd91dbc2bfc4973f175f42c
salemm <s.e.jugr@gmail.com>
2019-10-23 17:54:17 -0500

    :zap: 2 :skull:

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /**
  * @function  findSum
- * 1. Given an array with n integers and a number k, can you determine if there is a pair of 2 elements that
-sums to k? Create a simple program that will return the indices of the first encounter of the correct 2
-numbers. You may manipulate the array if necessary*/
+ *  1. Given an array with n integers and a number k, can you determine if there is a pair of 2 elements that
+ *   sums to k? Create a simple program that will return the indices of the first encounter of the correct 2
+ *   numbers. You may manipulate the array if necessary
+ * */
 
 function  findSum(nums, k) {
     console.log(nums, k)
@@ -20,4 +21,26 @@ function  findSum(nums, k) {
     }
   }
   
-  console.log(findSum([12, 5, 8, 1, 7, 9, 22, 21, 4, 32, 1], 33))
+   findSum([12, 5, 8, 1, 7, 9, 22, 21, 4, 32, 1], 33)
+
+
+  /**
+   * @function countOccurences
+   *  2. Given a string, create a simple program that will return the number of occurrences of each letter. The
+   *  result should not ignore case.
+   *  
+   */
+
+  function countOccurences(word){
+    let counter = {}
+    let wordJoin = word.replace(/\s/g, "");
+    let spliten = wordJoin.toLowerCase().split('')
+     spliten.reduce((prev,actual) => {
+       prev[actual] = (prev[actual] || 0) +1
+       return prev
+     },counter)
+     console.log(counter)
+    }
+ 
+ 
+    countOccurences("Purple People Eater")
